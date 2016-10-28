97a729cf36ac15cf04c7298a8e18f640a1a01a1a
Jan Dillmann <j@n-dillmann.de>
2016-10-28 11:17:34 +0200

    Add user filter to post admin

--- app/models/concerns/post_admin.rb
+++ app/models/concerns/post_admin.rb
@@ -5,9 +5,19 @@ module PostAdmin
     rails_admin do
       object_label_method :title
 
+      field :user_id, :enum do
+        enum do
+          # bindings is nil, so the following line does not work:
+          # User.accessible_by(bindings[:controller].current_user).map { |user| [user.name, user.id] }
+          User.all.map { |user| [user.name, user.id] }
+        end
+      end
+
       list do
         field :id
-        field :user
+        field :user do
+          filterable false
+        end
         field :title
       end
 
